30bc50c17c447d409df781685aa3944b410c7c65
Rain <6rainforced9@gmail.com>
2024-09-24 21:43:46 +0300

    Added the Players and the ball

--- test01.py
+++ test01.py
@@ -6,7 +6,7 @@ winWidth = 1800
 winHeight = 1000
 screen = pygame.display.set_mode((winWidth, winHeight))
 
-playerHeight = 200
+playerHeight = 250
 playerThickness = 35
 
 player1PosX = 0
@@ -14,18 +14,56 @@ player1PosY = 425
 player2PosX = 1800 - playerThickness
 player2PosY = 425
 
+deltaTime = 0
+time0 = 0
+accelYUp = 0
+accelYDown = 0
+vel = 0
+friction = -0.12
+
+
+ballPosX = 875
+ballPosY = 475
 run = True
 
 while run:
+
+    
     screen.fill("black")
-# Drawing the objects on the screen
+    # Getting the time
+    time = pygame.time.get_ticks()
+    clock = pygame.time.Clock()
+    dt = clock.tick(60) * 0.001 * 144
+    print(dt)
+    time = time/1000
+
+    # Drawing the objects on the screen
     pygame.draw.rect(screen, "white", pygame.Rect(player1PosX, player1PosY, playerThickness, playerHeight))
     pygame.draw.rect(screen, "white", pygame.Rect(player2PosX, player2PosY, playerThickness, playerHeight))
-    pygame.draw.rect(screen, "white", pygame.Rect(player2PosX, player2PosY, playerThickness, playerHeight))
-    
-# Quit Mechanic
+    pygame.draw.rect(screen, "red", pygame.Rect(ballPosX, ballPosY, 50, 50))
+
+    # Player Movement Mechanic
+    keys = pygame.key.get_pressed()
+    accelY = 0
+    if keys[pygame.K_w] and player1PosY > 0: 
+        accelY -= .3
+        if vel >= 2:
+            vel = 2
+    accelY += vel * friction  
+    player1PosY += vel *dt + (accelY * (dt * dt))/2
+    vel =  accelY * dt
+
+    if keys[pygame.K_s] and player1PosY < winHeight - playerHeight:
+        accelY += .3
+        if vel >= 2:
+            vel = 2
+    accelY += vel * friction  
+    vel =  accelY * dt
+    player1PosY += vel *dt + (accelY * (dt * dt))/2
+    # Quit Mechanic
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     pygame.display.update()
+
 pygame.quit()
