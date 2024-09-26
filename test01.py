96651e2d316a45553af27949b6b5ba8e7e69449b
Rain <6rainforced9@gmail.com>
2024-09-26 22:06:48 +0300

    Made the game work

--- test01.py
+++ test01.py
@@ -6,48 +6,70 @@ winWidth = 1400
 winHeight = 800
 screen = pygame.display.set_mode((winWidth, winHeight))
 
-playerHeight = 180
-playerThickness = 35
+playerSize = pygame.Vector2(35, 180)
 
-player1Pos = pygame.Vector2(20, (winHeight - playerHeight)/2)
-player2Pos = pygame.Vector2(winWidth - playerThickness - 20, (winHeight - playerHeight)/2)
+player1Pos = pygame.Vector2(20, (winHeight - playerSize.y)/2)
+player2Pos = pygame.Vector2(winWidth - playerSize.x - 20, (winHeight - playerSize.y)/2)
 
-ballHeight = 50
-ballWidth = 50
-ballPos = pygame.Vector2((winWidth - ballWidth)/2, (winHeight - ballHeight)/2)
+ballRadius = 30
+ballPos = pygame.Vector2((winWidth)/2, (winHeight)/2)
 ballDirection = pygame.Vector2(1, 1)
-ballVel = 5
-ballVelY = 1
+ballSpeed = pygame.Vector2(10, 10)
 
 playerAccelRate = 2
 friction = .8
 vel1 = 0
 vel2 = 0
 
- # Ball Behaviour
+scoreP1 = 0
+scoreP2 = 0
+run = True
 
-def ballMovement(ballX, ballY, ballSpeed, ballSpeedY, ballDirectionX, ballDirectionY):
-    if ballDirectionX == 1 and ballX < winWidth - ballWidth:
+
+# Text
+font = pygame.font.Font('gameFont.ttf', 42)
+
+# Ball Behaviour
+def ballMovement(ballX, ballY, ballSpeed, ballSpeedY, ballDirectionX, ballDirectionY, scoreP1, scoreP2):
+    if ballDirectionX == 1 and ballX < winWidth - ballRadius:
         ballX += ballSpeed
-    elif ballDirectionX == 1 and ballX >= winWidth - ballWidth:
+    elif ballDirectionX == 1 and ballX >= winWidth - ballRadius:
         ballDirectionX *= -1
-    if ballDirectionY == 1 and ballY > 0:
-        ballY -= ballSpeedY
-    elif ballDirectionY == 1 and ballY <= 0:
-        ballDirectionY *= -1
-    if ballDirectionX == -1 and ballX > 0:
+        scoreP1 += 1
+        ballX = winWidth/2
+        ballY = winHeight/2
+
+    if ballDirectionX == -1 and ballX > ballRadius:
         ballX -= ballSpeed
-    elif ballDirectionX == -1 and ballX <= 0:
+    elif ballDirectionX == -1 and ballX <= ballRadius:
         ballDirectionX *= -1
-    if ballDirectionY == -1 and ballY < winHeight - ballHeight:
-        ballY += ballSpeedY
-    elif ballDirectionY == -1 and ballY >= winHeight - ballHeight:
+        scoreP2 += 1
+        ballX = winWidth/2
+        ballY = winHeight/2
+
+    if ballDirectionY == 1 and ballY > ballRadius:
+        ballY -= ballSpeedY
+    elif ballDirectionY == 1 and ballY <= ballRadius:
         ballDirectionY *= -1
-    return ballX, ballY, ballDirectionX, ballDirectionY
+        
+    if ballDirectionY == -1 and ballY < winHeight - ballRadius:
+        ballY += ballSpeedY
+    elif ballDirectionY == -1 and ballY >= winHeight - ballRadius:
+        ballDirectionY *= -1
+
+    return ballX, ballY, ballDirectionX, ballDirectionY, scoreP1, scoreP2
+
+def BallIsColliding(playerPosition, playerSize, ballRadius, ballPosition):
+    collisionX = max(playerPosition.x, min(ballPosition.x, playerPosition.x + playerSize.x))
+    collisionY = max(playerPosition.y, min(ballPosition.y, playerPosition.y + playerSize.y))
+
+    distanceX = ballPosition.x - collisionX
+    distanceY = ballPosition.y - collisionY
+
+    return (distanceX ** 2 + distanceY ** 2) <= (ballRadius ** 2)
 
 # Main Game loop --->
 
-run = True
 while run:
     
     screen.fill("black")
@@ -57,35 +79,40 @@ while run:
 
     # Drawing the objects on the screen
 
-    pygame.draw.circle(screen, "white", (winWidth/2, winHeight/2), winHeight/4)
-    pygame.draw.circle(screen, "black", (winWidth/2, winHeight/2), winHeight/4 - 2)
+    pygame.draw.circle(screen, "white", (winWidth/2, winHeight/2), 150)
+    pygame.draw.circle(screen, "black", (winWidth/2, winHeight/2), 150 - 4)
 
-    pygame.draw.circle(screen, "white", (150, winHeight/2), winHeight * 0.2 + 1.5)
-    pygame.draw.circle(screen, "black", (150, winHeight/2), winHeight * 0.2 - 1)
-    pygame.draw.rect(screen, "black", pygame.Rect(0, winHeight * 0.3, 150, winHeight * 0.4 + 2))
-    pygame.draw.rect(screen, "white", pygame.Rect(0, winHeight * 0.3, 150, 2))
-    pygame.draw.rect(screen, "white", pygame.Rect(0, winHeight * 0.7, 150, 2))
+    pygame.draw.rect(screen, "white", pygame.Rect(winWidth/2 - 2, 0, 4, winHeight))
 
-    pygame.draw.circle(screen, "white", (winWidth - 150, winHeight/2), winHeight * 0.2 + 2)
-    pygame.draw.circle(screen, "black", (winWidth - 150, winHeight/2), winHeight * 0.2)
-    pygame.draw.rect(screen, "black", pygame.Rect(winWidth - 150, winHeight * 0.3, 150, winHeight * 0.4 + 2))
-    pygame.draw.rect(screen, "white", pygame.Rect(winWidth - 150, winHeight * 0.3, 150, 1.5))
-    pygame.draw.rect(screen, "white", pygame.Rect(winWidth - 150, winHeight * 0.7, 150, 2))
+    pygame.draw.rect(screen, "white", pygame.Rect(player1Pos.x, player1Pos.y, playerSize.x, playerSize.y))
+    pygame.draw.rect(screen, "white", pygame.Rect(player2Pos.x, player2Pos.y, playerSize.x, playerSize.y))
+    pygame.draw.circle(screen, "red", ballPos, ballRadius)
 
-    pygame.draw.rect(screen, "white", pygame.Rect(winWidth/2 - 2, 0, 2, winHeight))
-
-    pygame.draw.rect(screen, "white", pygame.Rect(player1Pos.x, player1Pos.y, playerThickness, playerHeight))
-    pygame.draw.rect(screen, "white", pygame.Rect(player2Pos.x, player2Pos.y, playerThickness, playerHeight))
-    pygame.draw.rect(screen, "red", (pygame.Rect(ballPos.x, ballPos.y, ballWidth, ballHeight)))
-
-    
-    
-
-
+    # Score
+    text = font.render(str(scoreP1) + "             " + str(scoreP2), True, "white")
+    textSurface = text.get_rect()
+    textSurface.center = ( winWidth/2, 50)
+    screen.blit(text, textSurface)
+    # Game win/lose handling
+    if scoreP1 > 5:
+        GameText1 = font.render("Player 1 Won!", True, "white")
+        blackBG = GameText1.get_rect()
+        screen.blit(blackBG, (0,0))
+    elif scoreP2 > 5:
+        GameText2 = font.render("Player 2 Won!", True, "white")
+        blackBG = GameText2.get_rect()
+        screen.blit(blackBG, (0,0))
     # Ball Behaviour
 
-    ballPos.x, ballPos.y, ballDirection.x, ballDirection.y = ballMovement(ballPos.x, ballPos.y, ballVel, ballVelY, ballDirection.x, ballDirection.y)
-
+    ballPos.x, ballPos.y, ballDirection.x, ballDirection.y, scoreP1, scoreP2 = ballMovement(ballPos.x, ballPos.y, ballSpeed.x, ballSpeed.y, ballDirection.x, ballDirection.y, scoreP1, scoreP2)
+    
+    if BallIsColliding(player1Pos, playerSize, ballRadius, ballPos):
+        ballPos.x = player1Pos.x + playerSize.x + ballRadius
+        ballDirection.x *= -1
+    if BallIsColliding(player2Pos, playerSize, ballRadius, ballPos):
+        ballPos.x = player2Pos.x - ballRadius
+        ballDirection.x *= -1
+    
 
     # Player Movement Mechanic
     accel1 = 0
@@ -93,11 +120,11 @@ while run:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w] and player1Pos.y > 0:
         accel1 -= playerAccelRate
-    if keys[pygame.K_s] and player1Pos.y < winHeight - playerHeight:
+    if keys[pygame.K_s] and player1Pos.y < winHeight - playerSize.y:
         accel1 += playerAccelRate
     if keys[pygame.K_UP] and player2Pos.y > 0:
         accel2 -= playerAccelRate
-    if keys[pygame.K_DOWN] and player2Pos.y < winHeight - playerHeight:
+    if keys[pygame.K_DOWN] and player2Pos.y < winHeight - playerSize.y:
         accel2 += playerAccelRate
     accel1 += vel1 * friction
     accel2 += vel2 * friction
@@ -106,20 +133,19 @@ while run:
     player1Pos.y += vel1 * dt + accel1/2 * (dt*dt)
     player2Pos.y += vel2 * dt + accel2/2 * (dt*dt)
     # Player 2 Constraints
-    if player1Pos.y >= winHeight - playerHeight:
-        player1Pos.y = winHeight - playerHeight
+    if player1Pos.y >= winHeight - playerSize.y:
+        player1Pos.y = winHeight - playerSize.y
     if player1Pos.y <= 0:
         player1Pos.y = 0
     # Player 2 Constraints
-    if player2Pos.y >= winHeight - playerHeight:
-        player2Pos.y = winHeight - playerHeight
+    if player2Pos.y >= winHeight - playerSize.y:
+        player2Pos.y = winHeight - playerSize.y
     if player2Pos.y <= 0:
         player2Pos.y = 0
 
     # Quit Mechanic
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            pygame.quit()
     pygame.display.update()
 
-pygame.quit()
